0e802b574436d55599fc45e913cde8f87ee3e78d
Evan Tan <tcyevan@gmail.com>
2021-03-08 15:43:55 +0800

    Pretty formatting using VSCode extensions

--- lab_1/q5.m
+++ lab_1/q5.m
@@ -3,43 +3,44 @@ format short
 format compact
 %% Midtread vs midrise
 %{
-- Midrise output is more even, has lowest error for input levels at delta/2,
+- Midrise output is more even, has lowest error for input levels at delta / 2,
 and largest error for input levels at delta.
 - Midtread has lowest error for input levels at delta, and largest error
-for input levels at delta/2
-%}
 
-%% Vector that represents time-varying sinusoid
-    testFrequency           = 1e3;                  % 1kHz
-    sampleFrequency         = testFrequency*8;      % sampling at 8kHz (greater than Nyquist rate)
-    samplePeriod            = 1/sampleFrequency;
-    amplitude               = 1;
-    duration                = 2;
+for input levels at delta / 2
+    %}
 
-    samples                 = sampleFrequency * duration;
-    timeVector              = (0:samples-1) * samplePeriod;
-    signalVector            = amplitude * sin(2*pi * testFrequency * timeVector);
+    %% Vector that represents time-varying sinusoid
+    testFrequency = 1e3; % 1kHz
+    sampleFrequency = testFrequency * 8; % sampling at 8kHz (greater than Nyquist rate)
+    samplePeriod = 1 / sampleFrequency;
+    amplitude = 1;
+    duration = 2;
+
+    samples = sampleFrequency * duration;
+    timeVector = (0:samples - 1) * samplePeriod;
+    signalVector = amplitude * sin(2 * pi * testFrequency * timeVector);
     figure
     hold on
     plot(signalVector)
 
-%% SNR as a function of m (bits per sample)
+    %% SNR as a function of m (bits per sample)
     count = 1;
     bpsVector = 2:16; % bits per sample vector
     SNRVector = zeros(size(bpsVector));
 
-    for m=bpsVector(1):bpsVector(end)
-        levels              = 2^m-1; % odd number of levels
-        quantVector         = 2*round(signalVector*(levels-1)/2) / (levels-1); % midtread quantiser
-        quantNoiseVector    = signalVector - quantVector;
-        Ps                  = 1/length(signalVector) * sum(signalVector.^2);
-        Pn                  = 1/length(quantNoiseVector) * sum(quantNoiseVector.^2);
-        SNR_calc            = 10*log10(Ps / Pn); % convert to dB
-        SNRVector(count)    = SNR_calc; % ensure our indices start from 1
-        count               = count + 1;
+    for m = bpsVector(1):bpsVector(end)
+        levels = 2^m - 1; % odd number of levels
+        quantVector = 2 * round(signalVector * (levels - 1) / 2) / (levels - 1); % midtread quantiser
+        quantNoiseVector = signalVector - quantVector;
+        Ps = 1 / length(signalVector) * sum(signalVector.^2);
+        Pn = 1 / length(quantNoiseVector) * sum(quantNoiseVector.^2);
+        SNR_calc = 10 * log10(Ps / Pn); % convert to dB
+        SNRVector(count) = SNR_calc; % ensure our indices start from 1
+        count = count + 1;
     end
 
-%% Result
+    %% Result
     % uncomment this to play sound
     %sound(quantVector,sampleFrequency)
 
@@ -47,10 +48,10 @@ for input levels at delta/2
     axis([0 20 -1 1])
     hold off
     title('Plot of signal vector and quantised vector')
-    legend('signal','quant')
+    legend('signal', 'quant')
 
     figure
-    plot(bpsVector,SNRVector)
+    plot(bpsVector, SNRVector)
     title('Plot of SNR against m, bits per sample')
 
     % assume a line of best fit through SNR oops
--- lab_1/q6.m
+++ lab_1/q6.m
@@ -1,40 +1,41 @@
 clear all; close all; clc;
 
 %% Audio sample
-    %filename=['ShortClip_Shine On.wav'];
-    filename=['12secSpeechSample - Speeches That Changed The World - Intro.wav'];
+%filename=['ShortClip_Shine On.wav'];
+filename = ['12secSpeechSample - Speeches That Changed The World - Intro.wav'];
 
-    [signalVector,sampleFrequency]=audioread(filename);
-    [samples,x]=size(signalVector);
-    if x>1
-        disp('Warning: This is not a mono recording')
-    end
-    duration = samples/sampleFrequency;
-    display=[num2str(duration),' second audio file, sampled at ', num2str(sampleFrequency),' Hz'];
-    disp(display);
+[signalVector, sampleFrequency] = audioread(filename);
+[samples, x] = size(signalVector);
+
+if x > 1
+    disp('Warning: This is not a mono recording')
+end
+
+duration = samples / sampleFrequency;
+display = [num2str(duration), ' second audio file, sampled at ', num2str(sampleFrequency), ' Hz'];
+disp(display);
 
 %% SNR as a function of m (bits per sample)
 count = 1;
 bpsVector = 2:16; % bits per sample vector
 SNRVector = zeros(size(bpsVector));
 
-for m=bpsVector(1):bpsVector(end)
-    levels              = 2^m-1;                                           % odd number of levels
-    quantVector         = 2*round(signalVector*(levels-1)/2) / (levels-1);
-    quantNoiseVector    = signalVector - quantVector;
-    Ps                  = 1/length(signalVector) * sum(signalVector.^2);
-    Pn                  = 1/length(quantNoiseVector) * sum(quantNoiseVector.^2);
-    SNR_calc            = 10*log10(Ps / Pn);
-    SNRVector(count)      = SNR_calc;
-    count               = count + 1;
+for m = bpsVector(1):bpsVector(end)
+    levels = 2^m - 1; % odd number of levels
+    quantVector = 2 * round(signalVector * (levels - 1) / 2) / (levels - 1);
+    quantNoiseVector = signalVector - quantVector;
+    Ps = 1 / length(signalVector) * sum(signalVector.^2);
+    Pn = 1 / length(quantNoiseVector) * sum(quantNoiseVector.^2);
+    SNR_calc = 10 * log10(Ps / Pn);
+    SNRVector(count) = SNR_calc;
+    count = count + 1;
 end
 
-
 %% Result
-    % uncomment this to play sound
-    %sound(quantVector,sampleFrequency)
+% uncomment this to play sound
+%sound(quantVector,sampleFrequency)
 
-    plot(bpsVector,SNRVector)
+plot(bpsVector, SNRVector)
 
-    coefficients = polyfit(bpsVector, SNRVector, 1)
-    slope = coefficients(1)
+coefficients = polyfit(bpsVector, SNRVector, 1)
+slope = coefficients(1)
