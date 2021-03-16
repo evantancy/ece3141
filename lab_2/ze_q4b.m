5469b826e44f84d6bbf1a5e27ecb532234d51200
zefeiteo <66097256+zefeiteo@users.noreply.github.com>
2021-03-16 18:12:40 +1100

    Formatted q4b m file

--- lab_2/ze_q4b.m
+++ lab_2/ze_q4b.m
@@ -1,5 +1,8 @@
 clear all; close all; clc;
+format short; 
+format compact;
 
+%% 
 HuffmanObject = importdata('HuffmanText.mat');
 char = HuffmanObject.AsciiChar;
 prob = HuffmanObject.AsciiProb;
@@ -13,7 +16,15 @@ prob = HuffmanObject.AsciiProb;
     minProb = min(prob);
     
 %% Encode/Decode
-    text = ('a "typical" text string reduced using Huffman coding?');
-    enco = huffmanenco(text, dict);
-    deco = huffmanenco(enco, dict);
-    equal = isequal(text,deco)
+    string = ('a "typical" text string reduced using Huffman coding?');
+    
+    string_binary = dec2bin(string);
+    num_string = numel(string_binary);
+    
+    enco = huffmanenco(string, dict);
+    d2b_enco = de2bi(enco);
+    num_enco = numel(enco);
+
+    comression = num_enco/num_string;
+    reduction = (num_string-num_enco)/num_string;
+    
